6dbb402b15ab56f6fce57dc8e9c6aabd4b768350
StevieBrooks <brookscodeblog@gmail.com>
2023-08-22 18:18:31 +0700

    functionality for multiInput search sorted

--- libs/js/main.js
+++ libs/js/main.js
@@ -19,9 +19,7 @@ window.onload = function() {
                         if(typeof item.GeoLocation === "string" && typeof item.year === "number" && typeof item["mass (g)"] === "number") {
                             csvResult.push(item);
                         }
-                    })
-                    console.log(csvResult);
-                    
+                    })                    
                 },
                 error: (error) => {
                     console.error("Error parsing CSV:", error.message);
@@ -51,15 +49,23 @@ let marker = null;
 
  /*====================SEARCH FUNCTIONS===================*/
 
+ const searchParameters = {
+    openQuery: null,
+    nameQuery: null,
+    centuryQuery: null
+ }
+
 /*MAIN SEARCH BAR*/
-let mainSearchInput = null;
-// mainSearchInput = document.querySelector(".main-search").value;
-        // const searchInput = mainSearchInput.trim().toLowerCase(); 
+// let mainSearchInput = null;
+// const mainSearch = document.querySelector(".main-search");
 
-function searchBar() {
-
-}
-
+// mainSearch.addEventListener("click", searchBar);
+// function searchBar() {
+//     nameInput.value = "";
+//     searchParameters.nameQuery = null;
+//     mainSearchInput = document.querySelector(".main-search").value.trim().toLowerCase();
+//     searchParameters.openQuery = mainSearchInput;
+// }
 
 
 /*NAME SEARCH INPUT*/
@@ -70,90 +76,122 @@ nameInput.addEventListener("change", nameInputFunction);
 
 function nameInputFunction() {
     nameChoice = Array.from(nameInput.value);
-    // [let1, let2, let3, let4, let5] = nameChoice;
-    return nameChoice;
+    console.log(nameChoice);
+    // searchParameters.openQuery = null;
+    searchParameters.nameQuery = nameChoice;
 }
+
 
 /*CENTURY SEARCH INPUT*/
-// const centurySelect = document.querySelector(".century-select");
+const centurySelect = document.querySelector(".century-select");
 
-// centurySelect.addEventListener("change", centuryInputFunction);
+centurySelect.addEventListener("change", centuryInputFunction);
 
-// function centuryInputFunction() {
-//     const yearVals = csvResult.map(item => item.year).filter(value => typeof value === "number");
-//                     console.log(yearVals);
-//                     const max = Math.max(...yearVals);
-//                     const min = Math.min(...yearVals);
-//                     console.log(max);
-//                     console.log(min);
-//     console.log(centurySelect.value);
-// } RETURN TO THIS LATER!
+function centuryInputFunction() {
 
+    console.log(centurySelect.value);
 
+    searchParameters.centuryQuery = centurySelect.value;
 
-/*TAILOR SEARCH*/
-function searchParams() {
-    const nameSelection = nameInputFunction();
-    console.log(nameSelection);
-
-    csvResult.forEach(item => {
-        const itemName = item.name.trim().toLowerCase(); 
-            // includes method naturally returns more results, but need to decide with team mates whether to use that or startsWith() method
-            for(letter of nameSelection) {
-                if(itemName.startsWith(letter)) {
-                    const markerIcon = L.icon({
-                        iconUrl: "assets/images/markerIcon.png",
-                        iconSize: [30, 30],
-                        iconAnchor: [50, 50],
-                        popupAnchor: [-35, -55]
-                    })
-                    
-                    marker = L.marker([item.reclat, item.reclong], {
-                        icon: markerIcon
-                    }).addTo(layerGroup);
-                
-                    const markerPopup = L.popup().setContent(`
-                        <ul class="popup-list" style="list-style: none;">
-                            <li class="popup-list-item"><strong>Id:</strong> ${item.id}</li>
-                            <li class="popup-list-item"><strong>Name:</strong> ${item.name}</li>
-                            <li class="popup-list-item"><strong>Record Class:</strong> ${item.recclass}</li>
-                            <li><strong>Mass (g):</strong> ${item["mass (g)"]}</li>
-                            <li><strong>Year of Impact:</strong> ${item.year}</li>
-                            <li><strong>Latitude:</strong> ${item.reclat}</li>
-                            <li><strong>Longitude:</strong> ${item.reclong}</li>
-                        </ul>
-                        
-                        
-                        
-                    `);
-                    marker.bindPopup(markerPopup).addTo(map);
-                }
-            }
-            
-            
-
-        // }
-    });
-    
-}
+} 
 
 
 /*SEARCH BUTTON*/
+const searchForm = document.querySelector(".search-form");
 const mainSearchButton = document.querySelector(".main-search-btn");
 mainSearchButton.addEventListener("click", searchNow)
 
 function searchNow() {
 
-    layerGroup.clearLayers();
-    searchParams();
+    console.log(searchParameters);
 
+    layerGroup.clearLayers();
+    searchForm.reset();
+    filterOne();
+
+}
+
+
+function filterOne() {
+    
+    const filterTwoItems = [];
+
+    csvResult.forEach(item => {
+        const itemName = item.name.trim().toLowerCase(); 
+        if(itemName.startsWith(searchParameters.nameQuery[0]) || itemName.startsWith(searchParameters.nameQuery[1]) || itemName.startsWith(searchParameters.nameQuery[2])) {
+            filterTwoItems.push(item);
+        }
+    })
+
+    filterTwo(filterTwoItems);
+
+}
+
+
+function filterTwo(itemsToFilter) {
+
+    const itemsForDisplay = [];
+    const lowEnd = searchParameters.centuryQuery.slice(0, 4);
+    const highEnd = searchParameters.centuryQuery.slice(-4);
+
+    itemsToFilter.forEach(item => {
+        if(item.year >= lowEnd && item.year <= highEnd) {
+            itemsForDisplay.push(item);
+        }
+    })
+
+    displayItems(itemsForDisplay);
+
+}
+
+
+function displayItems(finalArray) {
+
+    finalArray.forEach(item => {
+
+        const markerIcon = L.icon({
+            iconUrl: "assets/images/markerIcon.png",
+            iconSize: [30, 30],
+            iconAnchor: [50, 50],
+            popupAnchor: [-35, -55]
+        })
+        
+        marker = L.marker([item.reclat, item.reclong], {
+            icon: markerIcon
+        }).addTo(layerGroup);
+    
+        const markerPopup = L.popup().setContent(`
+            <ul class="popup-list" style="list-style: none;">
+                <li class="popup-list-item"><strong>Id:</strong> ${item.id}</li>
+                <li class="popup-list-item"><strong>Name:</strong> ${item.name}</li>
+                <li class="popup-list-item"><strong>Record Class:</strong> ${item.recclass}</li>
+                <li><strong>Mass (g):</strong> ${item["mass (g)"]}</li>
+                <li><strong>Year of Impact:</strong> ${item.year}</li>
+                <li><strong>Latitude:</strong> ${item.reclat}</li>
+                <li><strong>Longitude:</strong> ${item.reclong}</li>
+            </ul>                       
+        `);
+
+        marker.bindPopup(markerPopup).addTo(map);
+
+    })
 }
 
 
 
 
-/*
-1. store inputs from each input as variables
-2. return those variables from each input function to be used as params for searchButton (remember, searchInput cancels out name input when active and vice versa)
-3. searchButton functionality
-*/
+
+
+
+
+
+
+
+
+    // const yearVals = csvResult.map(item => item.year).filter(value => typeof value === "number");
+    //                 console.log(yearVals);
+    //                 const max = Math.max(...yearVals);
+    //                 const min = Math.min(...yearVals);
+    //                 console.log(max);
+    //                 console.log(min);
+    // console.log(centurySelect.value);
